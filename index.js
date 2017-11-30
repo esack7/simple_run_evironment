1c01187fcc743b76643892a1ba6537f8bf0675dc
esack7 <isaacheist@gmail.com>
2017-11-30 12:02:38 -0800

    set up simple run environment

---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+function solution(n) {
+  console.log(n);
+}
+
+solution('hello world');
---
+++ index.test.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const testSolution = require('./index.js');
+require('jest');
+
+describe('testing the function solution', function () {
+  // test('What is being tested?', () => {
+  //   expect(testSolution()).toBe();
+  // });
+  test('What is truth?', () => {
+    expect(true).toBeTruthy();
+  });
+});
